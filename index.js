e4ecf143c58129da76b410b01c173702f90c4054
AdamWier <38018787+AdamWier@users.noreply.github.com>
2021-08-28 20:02:36 +0200

    Basic call to google calendar

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { google } = require('googleapis');
+const auth = new google.auth.GoogleAuth({
+  keyFile: './key.json',
+  scopes: ['https://www.googleapis.com/auth/calendar'],
+});
+const calendar = google.calendar({version: 'v3', auth});
+
 app.get('/mycalendar', (req, res) => {
-  res.send('Hello World!');
+    calendar.events.list({
+        calendarId: 'wier.adam@gmail.com',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+    }, (err, googleRes) => {
+        const events = googleRes.data.items
+        res.json(events);
+    })
 })
 
 app.listen(port, () => {
